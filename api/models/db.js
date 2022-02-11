70f094ab3b1bcb2f6b973bb5c38ec597a2f0e3b1
Tawkir Ahmed <tawkira.786@gmail.com>
2022-02-11 11:37:34 +0530

    Update db.js
    removed mongo link;

--- api/models/db.js
+++ api/models/db.js
@@ -1,7 +1,6 @@
 require('./user');
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://testUser123:testUser123@justconnect-shard-00-00.d5dy6.mongodb.net:27017,justconnect-shard-00-01.d5dy6.mongodb.net:27017,justconnect-shard-00-02.d5dy6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-lmeam7-shard-0&authSource=admin&retryWrites=true&w=majority' 
-// 'mongodb+srv://testUser123:testUser123@justconnect.d5dy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const dbURI = '' 
 
 // mongoose.set('useCreateIndex', true);
 mongoose.connect(dbURI, {
